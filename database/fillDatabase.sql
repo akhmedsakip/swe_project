202d4113b2bdd93f98547481f04bc7e98c294c39
Timur <timur.rakhimzhan@nu.edu.kz>
2020-10-19 12:22:20 +0600

    database changed

---
+++ database/fillDatabase.sql
@@ -0,0 +1,71 @@
+INSERT INTO Hotels (Name, NumberOfFloors,
+                    NumberOfRooms, NumberOfFreeRooms,
+                    Country, City, Street, ZIPCode,
+                    MainHotelPicture, Description,
+                    StarsCount) VALUES (
+                                        'Hotel #1',
+                                        4,
+                                        200,
+                                        150,
+                                        'Country #1',
+                                        'City #1',
+                                        'Street #1',
+                                        'ZIP #1',
+                                        'images/logo.jpg',
+                                        'Description #1',
+                                        3
+                                       );
+
+INSERT INTO Hotels (Name, NumberOfFloors,
+                    NumberOfRooms, NumberOfFreeRooms,
+                    Country, City, Street, ZIPCode,
+                    MainHotelPicture, Description,
+                    StarsCount) VALUES (
+                                        'Hotel #2',
+                                        10,
+                                        1000,
+                                        300,
+                                        'Country #2',
+                                        'City #2',
+                                        'Street #2',
+                                        'ZIP #2',
+                                        'images/logo.jpg',
+                                        'Description #2',
+                                        5
+                                       );
+
+INSERT INTO Hotels (Name, NumberOfFloors,
+                    NumberOfRooms, NumberOfFreeRooms,
+                    Country, City, Street, ZIPCode,
+                    MainHotelPicture, Description,
+                    StarsCount) VALUES (
+                                        'Hotel #3',
+                                        7,
+                                        700,
+                                        100,
+                                        'Country #3',
+                                        'City #3',
+                                        'Street #3',
+                                        'ZIP #3',
+                                        'images/logo.jpg',
+                                        'Description #3',
+                                        4
+                                       );
+
+INSERT INTO Hotels (Name, NumberOfFloors,
+                    NumberOfRooms, NumberOfFreeRooms,
+                    Country, City, Street, ZIPCode,
+                    MainHotelPicture, Description,
+                    StarsCount) VALUES (
+                                        'Hotel #4',
+                                        3,
+                                        250,
+                                        100,
+                                        'Country #4',
+                                        'City #4',
+                                        'Street #4',
+                                        'ZIP #4',
+                                        'images/logo.jpg',
+                                        'Description #4',
+                                        4
+                                       );
