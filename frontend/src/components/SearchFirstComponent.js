f7941f582285d03ba738b042ffc40799f9f91412
Timur <timur.rakhimzhan@nu.edu.kz>
2020-11-06 04:54:01 +0600

    verstka made

--- frontend/src/components/SearchFirstComponent.js
+++ frontend/src/components/SearchFirstComponent.js
@@ -24,13 +24,19 @@ const useStyles = makeStyles({
         minWidth: 170,
     },
     root: {
-        maxWidth: 345,
-        margin: '1vmax'
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
     },
 
     link: {
         color: 'black',
         textDecoration: 'none',
+    },
+    row: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
     },
 
     description: {
@@ -47,6 +53,9 @@ const useStyles = makeStyles({
             outline: '1px solid slategrey'
         },
         paddingRight: "1rem"
+    },
+    marginBottom12: {
+        marginBottom: 12,
     }
 });
 
@@ -90,36 +99,41 @@ function SearchFirstComponent({ setSearchSuccess, cities }) {
         console.log(values, errors);
     }, [values, errors]);
     return (
-        <form onChange={handleChange} onBlur={handleBlur} onSubmit={handleSubmit}>
+        <form onChange={handleChange} onBlur={handleBlur} onSubmit={handleSubmit} className={classes.root}>
             <TextFieldWithError name="numPeople" label="Number of people" fullWidth
-                errorMessage={errors.numPeople}
-                error={touched.numPeople && !!errors.numPeople}
+                                errorMessage={errors.numPeople}
+                                error={touched.numPeople && !!errors.numPeople}
+                                className={classes.marginBottom12}
             />
             <TextFieldWithError name="country" label="Country" fullWidth
                 errorMessage={errors.country}
                 error={touched.country && !!errors.country}
+                className={classes.marginBottom12}
             />
-            <FormControl required className={classes.formControl} error={touched.city && !!errors.city}>
-                <InputLabel>City</InputLabel>
-                <Select native inputProps={{name: 'city'}}>
-                    <option aria-label="None" value="" />
-                    {cities.map(city => {
-                        return (
-                            <option key={city} value={city}>{city}</option>
-                        );
-                    })}
-                </Select>
-                {touched.city && !!errors.city ?
-                    <FormHelperText error>{errors.city}</FormHelperText> : null }
-            </FormControl>
-            <TextFieldWithError label="From" type="date" name="fromDate"
-                InputLabelProps={{
-                    shrink: true,
-                }} errorMessage={errors.fromDate} error={touched.fromDate && !!errors.fromDate} />
-            <TextFieldWithError label="To" type="date" name={"toDate"}
-                InputLabelProps={{
-                    shrink: true,
-                }} errorMessage={errors.toDate} error={touched.toDate && !!errors.toDate}/>
+            <div className={`${classes.row} ${classes.marginBottom12}`}>
+                <FormControl required className={classes.formControl} error={touched.city && !!errors.city}>
+                    <InputLabel>City</InputLabel>
+                    <Select native inputProps={{name: 'city'}}>
+                        <option aria-label="None" value="" />
+                        {cities.map(city => {
+                            return (
+                                <option key={city} value={city}>{city}</option>
+                            );
+                        })}
+                    </Select>
+                    {touched.city && !!errors.city ?
+                        <FormHelperText error>{errors.city}</FormHelperText> : null }
+                </FormControl>
+                <TextFieldWithError label="From" type="date" name="fromDate"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }} errorMessage={errors.fromDate} error={touched.fromDate && !!errors.fromDate} />
+                <TextFieldWithError label="To" type="date" name={"toDate"}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }} errorMessage={errors.toDate} error={touched.toDate && !!errors.toDate}/>
+            </div>
+
             <Button
                 disabled={!isValid}
                 variant="contained"
