ca1edd735cdb714a5da968d1d88891ec8e48d9f9
Islam Orazbek <48217587+islamorazbek@users.noreply.github.com>
2020-11-06 12:01:44 +0600

    home + about us

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,10 +1,21 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles({
   root: {
     display: 'flex',
-    alignItems: 'center'
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+  },
+  infoBlocK: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    textAlign: 'center',
+    color: 'white',
+    width: '80%',
   }
 });
 
@@ -12,11 +23,17 @@ function AboutUs() {
 
   const classes = useStyles();
 
+  const loremIpsum1 = "Amita Hotels - It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+
   return (
     <div className={classes.root}>
-      <h1>Here should be About Us</h1>
-    </div>
-    )
+      <div className={classes.infoBlocK}>
+        <Typography style={{ fontSize: '3.5vmin' }}>
+          {loremIpsum1}
+        </Typography>
+      </div>
+    </div >
+  );
 }
 
 export default AboutUs;
