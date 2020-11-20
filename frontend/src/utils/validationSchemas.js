142342dfde022bdf59ecedd522a71557943920e2
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2020-11-20 14:33:59 +0600

    maxDate for DateOfBirth is 'today'

--- frontend/src/utils/validationSchemas.js
+++ frontend/src/utils/validationSchemas.js
@@ -36,9 +36,10 @@ export const editInfoSchema = yup.object().shape({
     firstName: yup.string().required("First name is empty"),
     lastName: yup.string().required("Second name is empty"),
     email: yup.string().required("Email is empty").email("Email is invalid"),
-    dateOfBirth: yup.date("Date of birth is invalid").required("Date of birth is empty"),
+    dateOfBirth: yup.date("Date of birth is invalid").max(new Date()).required("Date of birth is empty"),
     gender: yup.string().required("Gender is empty"),
 });
+
 export const changePasswordSchema = yup.object().shape({
     oldPassword: yup.string().required("Password is empty").min(6, "Minimum length of password is 6"),
     newPassword: yup.string().required("Password is empty").min(6, "Minimum length of password is 6"),
