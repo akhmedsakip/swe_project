9587beab2c9f91756fbd8975ea40701c16ca7423
akhmedsakip <60207002+akhmedsakip@users.noreply.github.com>
2020-11-23 16:38:31 +0600

    photos transferred to local storage

--- database/fillData.sql
+++ database/fillData.sql
@@ -30,7 +30,7 @@ INSERT INTO hotel (Name,
                     'Seifullina',
                     '0050012',
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ornare ipsum vitae metus fermentum, a elementum ex maximus. Nam luctus mattis mi, ut aliquet diam cursus eget. Vestibulum tincidunt erat sed sapien auctor, a finibus lectus rhoncus. Aliquam volutpat purus diam, vitae vestibulum lectus luctus ut. Praesent viverra purus id tristique condimentum. Suspendisse potenti. Ut id felis vitae sapien vehicula finibus. Donec interdum volutpat pretium. Sed semper faucibus dapibus. Nulla tortor magna, iaculis eget sapien in, rhoncus malesuada erat.',
-                    'https://cf.bstatic.com/images/hotel/max1024x768/426/42696320.jpg',
+                    '/assets/hotelMainPictures/rixosAlmaty.jpg',
                      3
                    );
 
@@ -54,7 +54,7 @@ INSERT INTO hotel (Name,
                     'Kunayeva',
                     '010000',
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    'https://cf.bstatic.com/images/hotel/max1024x768/553/55308233.jpg',
+                    '/assets/hotelMainPictures/rixosNurSultan.jpg',
                     5
                    );
 
@@ -78,7 +78,7 @@ INSERT INTO hotel (Name,
                     'Aktau',
                     '130000',
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    'https://i2.wp.com/casp-geo.ru/wp-content/uploads/2020/09/Rixos-Water-World-Aktau.jpg?fit=1280%2C768',
+                    '/assets/hotelMainPictures/rixosAktau.jpg',
                     4
                    );
 
@@ -102,7 +102,7 @@ INSERT INTO hotel (Name,
                     'Borovoe',
                     '020000',
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    'https://pix10.agoda.net/hotelImages/401/401257/401257_15072611530033033820.jpg?s=1024x768',
+                    '/assets/hotelMainPictures/rixosBorovoe.jpg',
                     4
                    );
 
@@ -119,7 +119,7 @@ INSERT INTO room_type (HotelID,
 	                            20,
 	                            25,
 	                            2,
-	                            'https://cf.bstatic.com/images/hotel/max1024x768/426/42696338.jpg',
+	                            '/assets/roomTypePictures/standard.jpg',
 	                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 	                           );
 
@@ -136,7 +136,7 @@ INSERT INTO room_type (HotelID,
                                 40,
                                 100,
                                 3,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696356.jpg',
+                                '/assets/roomTypePictures/luxe.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -153,7 +153,7 @@ INSERT INTO room_type (HotelID,
                                 20,
                                 25,
                                 2,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696338.jpg',
+                                '/assets/roomTypePictures/standard.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -170,7 +170,7 @@ INSERT INTO room_type (HotelID,
                                 40,
                                 100,
                                 3,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696356.jpg',
+                                '/assets/roomTypePictures/luxe.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -187,7 +187,7 @@ INSERT INTO room_type (HotelID,
                                 20,
                                 25,
                                 2,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696338.jpg',
+                                '/assets/roomTypePictures/standard.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -204,7 +204,7 @@ INSERT INTO room_type (HotelID,
                                 40,
                                 100,
                                 3,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696356.jpg',
+                                '/assets/roomTypePictures/luxe.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -221,7 +221,7 @@ INSERT INTO room_type (HotelID,
                                 20,
                                 25,
                                 2,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696338.jpg',
+                                '/assets/roomTypePictures/standard.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
@@ -238,7 +238,7 @@ INSERT INTO room_type (HotelID,
                                 40,
                                 100,
                                 3,
-                                'https://cf.bstatic.com/images/hotel/max1024x768/426/42696356.jpg',
+                                '/assets/roomTypePictures/luxe.jpg',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                                );
 
